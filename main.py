9fa3e18c5b0396c08cda9a86cba50b518137d4eb
renG <rsandis96@gmail.com>
2026-01-18 00:02:10 +0200

    Implemented target consumption and partially fixed snake - food interaction.

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+"""
+List of things "To Do" or to consider in unorderly manner:
+----------------------------------------------------------
+[] Put together "Menu" system
+[] Solidify FSM logic
+[] Implement better timers? instead of using "sleep" - research needed
+[] Make "World" class handle all collisions
+[] Refactor "Border" class
+[/] Re-implement "Target" consumption logic with "Target" staying ignorant
+[] Refine all "Game Over" conditions and events related to it (Do I need this with
+   with "World" class present?)
+[] Create "Joystick" class and move it's function there ?
+[] Implement proper "Score" handler
+[] Implement additional buttons for "Menu" and/or "Reset"?
+[x] Fix snek vs food interaction (coordinate mismatch)
+"""
+
 import ssd1306
 import random
 from machine import Pin, ADC, I2C
@@ -26,7 +43,6 @@ class Joystick_Events:
 #######################
 # ---- Constants ---- #
 #######################
-
 JOY_X = 26
 JOY_Y = 27
 JOY_BTN = 22
@@ -37,7 +53,7 @@ DISPLAY_WIDTH = 128
 DISPLAY_HEIGHT = 64
 DISPLAY_ADDRESS = 0x3c
 
-BODY_SIZE = 3
+BODY_SIZE = 2
 
 ######################
 # ---- Hardware ---- #
@@ -63,25 +79,26 @@ class Snek_Body:
         # Initialize with one segment at the start location
         self.segments = [(self.ctrl.x, self.ctrl.y)]
         self.length = 5
-        self.size = 3
-
+        # self.size = 2
+        self.a = None
+        self.b = None
+        
     def update(self):
         # Add current head position
         self.segments.insert(0, (self.ctrl.x, self.ctrl.y))
-        # print(self.segments)
-       
+
         # Trim tail
         while len(self.segments) > self.length:
+            self.a, self.b = self.segments[-1] # Replaces refresh logic
             self.segments.pop()
 
     def draw(self, display):
         for seg in self.segments:
-            display.fill_rect(seg[0], seg[1], self.size, self.size, 1)
+            display.fill_rect(seg[0], seg[1], BODY_SIZE, BODY_SIZE, 1)
         # Replaces "whole display" refreshing
-        a, b = self.segments[-1]
-        display.fill_rect(a, b, self.size, self.size, 0)
+        if self.a is not None:
+            display.fill_rect(self.a, self.b, BODY_SIZE, BODY_SIZE, 0)
         
-    
     def body_collision(self):
         head = self.segments[0]
         body = self.segments[1:]
@@ -95,69 +112,29 @@ class Snek_Control:
     def __init__(self, starting_x=0, starting_y=0, max_x=128, max_y=64):
         self.x = starting_x
         self.y = starting_y
-        self.dx = 3
+        self.dx = BODY_SIZE
         self.dy = 0
         self.max_x = max_x
         self.max_y = max_y
         self.vector_dict = {
-        "left": (-3, 0),
-        "right": (3, 0),
-        "up": (0, -3),
-        "down": (0, 3)
+        "left": (-BODY_SIZE, 0),
+        "right": (BODY_SIZE, 0),
+        "up": (0, -BODY_SIZE),
+        "down": (0, BODY_SIZE)
     }
    
-    # My Closure attempt - as I learned, this is more memory heavy
-    # def control_test(self, direction):
-    #     print(direction)
-    #     def vector_swap():
-    #         a, b = self.vector_dict[direction]
-    #         if self.dx + a == 0 and self.dy + b == 0:
-    #             return None
-    #         else:
-    #             self.dx, self.dy = self.vector_dict[direction]
-    #     return vector_swap
-
-    # Trying this - less memory heavy (wrapper ?)
     @staticmethod
     def vector_swap(ctrl, direction):
         a, b = ctrl.vector_dict[direction]
         if ctrl.dx + a != 0 or ctrl.dy + b != 0:
             ctrl.dx, ctrl.dy = a, b
 
-    # Old Movement Logic
-    # def move_x_left(self):
-    #     if self.dx == 3: # Logic for restricting movement on same axis
-    #         return None
-    #     else:
-    #         self.dx = -3 # More Pythonic way would be: self.dx, self.dy = -3, 0
-    #         self.dy = 0
-
-    # def move_x_right(self):
-    #     if self.dx == -3:
-    #         return None
-    #     else:
-    #         self.dx = 3
-    #         self.dy = 0
-
-    # def move_y_up(self):
-    #     if self.dy == 3:
-    #         return None
-    #     else:
-    #         self.dx = 0
-    #         self.dy = -3
-
-    # def move_y_down(self):
-    #     if self.dy == -3:
-    #         return None
-    #     else:
-    #         self.dx = 0
-    #         self.dy = 3
-
     def update_pos(self):
         self.x += self.dx
         self.y += self.dy
 
-class Border:
+# Refactor this class into "Obstacles". Borders, walls...etc.
+class Obstacle:
     def __init__(self, outter_x, outter_y, inner_x, inner_y, outter_offset, inner_offset):
         self.outter_x = outter_x
         self.outter_y = outter_y
@@ -166,16 +143,20 @@ class Border:
         self.outter_offset = outter_offset
         self.inner_offset = inner_offset
     
-    def border_draw(self, display):
+    def draw(self, display):
         display.fill_rect(self.outter_offset, self.outter_offset, self.outter_x, self.outter_y, 1)
         display.fill_rect(self.inner_offset, self.inner_offset, self.inner_x, self.inner_y, 0)
 
 class Target:
-    def __init__(self, border): #Used to have snek_body as variable
+    def __init__(self, obstacle): #Used to have snek_body as variable
         # self.body = snek_body
-        self.bounds = border
+        self.bounds = obstacle
         self.rand_x = 0
         self.rand_y = 0
+        self.min_x = self.bounds.inner_offset + 1
+        self.max_x = self.bounds.inner_x - BODY_SIZE
+        self.min_y = self.bounds.inner_offset + 1
+        self.max_y = self.bounds.inner_y - BODY_SIZE
         self.exists = False
     
     # Randomizes coordinates of Target within "Game Field" bounds
@@ -187,8 +168,10 @@ class Target:
             #     self.rand_y = random.randrange(self.bounds.inner_offset, self.bounds.inner_y)
             #     # Checks for overlap between target and snake
             #     if (self.rand_x, self.rand_y) not in self.body.segments: break
-            self.rand_x = random.randrange(self.bounds.inner_offset, self.bounds.inner_x)
-            self.rand_y = random.randrange(self.bounds.inner_offset, self.bounds.inner_y)
+            # self.rand_x = random.randrange(self.bounds.inner_offset, self.bounds.inner_x, 2)
+            # self.rand_y = random.randrange(self.bounds.inner_offset, self.bounds.inner_y)
+            self.rand_x = random.randrange(self.min_x, self.max_x, BODY_SIZE)
+            self.rand_y = random.randrange(self.min_y, self.max_y, BODY_SIZE)
             self.exists = True
     
     def target_draw(self, display):  
@@ -229,7 +212,23 @@ class World: # The all knowing
     something like "obstacles" class, I suddenly don't have to worry about re-writing
     Target class in order to avoid from it spawning on the occupied pixels.
     """
-    place_holder = None
+    def __init__(self, snek_body, obstacle, target):
+        self.snek_body = snek_body
+        self.obstacle = obstacle
+        self.target = target
+
+    def world_test(self):
+        # Major logic issue: Snek moves by 3pixles, if Target is at, lets say x=32
+        # and snek is at x=30, with the next "step", it would go over the food at
+        # x=33 and not register that it was consumed, as I am matching exact (x, y)
+        head_pos = self.snek_body.segments[0]
+        target_pos = (self.target.rand_x, self.target.rand_y)
+        print(head_pos, " ", target_pos)
+
+        if head_pos == target_pos:
+            print(f"Target was consumed at: {head_pos}, {target_pos}")
+            self.target.exists = False
+            self.snek_body.length += 1
 
 
 #######################
@@ -271,32 +270,19 @@ def get_joystick_event():
 ###########################
 # ---- Class Objects ---- #
 ###########################
-snek_control = Snek_Control(starting_x=10, starting_y=27)
+snek_control = Snek_Control(starting_x=10, starting_y=26)
 snek_body = Snek_Body(snek_control)
-border = Border(outter_x=128, outter_y=56, inner_x=124,
-                inner_y=52, outter_offset=0, inner_offset=2)
-game_over = Game_Over(snek_control, border)
-target = Target(border) # Used to have snek_body as variable
-world = World(border, target)
+obstacle = Obstacle(outter_x=128, outter_y=55, inner_x=126,
+                inner_y=52, outter_offset=0, inner_offset=1)
+game_over = Game_Over(snek_control, obstacle)
+target = Target(obstacle) # Used to have snek_body as variable
+world = World(snek_body, obstacle, target)
 
 #########################
 # ---- Actions Map ---- #
 #########################
-# Original Actions Map
-# actions = {
-#     Joystick_Events.LEFT: snek_control.move_x_left,
-#     Joystick_Events.RIGHT: snek_control.move_x_right,
-#     Joystick_Events.UP: snek_control.move_y_up,
-#     Joystick_Events.DOWN: snek_control.move_y_down
-# }
-# Actions list for Closure attempt
-# actions = {
-#     Joystick_Events.LEFT: snek_control.control_test("left"),
-#     Joystick_Events.RIGHT: snek_control.control_test("right"),
-#     Joystick_Events.UP: snek_control.control_test("up"),
-#     Joystick_Events.DOWN: snek_control.control_test("down")
-# }
-# Actions list for Staticmethod Wrapper ?
+
+# Actions list for Staticmethod Wrapper
 actions = {
     Joystick_Events.LEFT: lambda: Snek_Control.vector_swap(snek_control, "left"),
     Joystick_Events.RIGHT: lambda: Snek_Control.vector_swap(snek_control, "right"),
@@ -367,7 +353,7 @@ random.seed(joy_x.read_u16() + joy_y.read_u16() + ticks_us())
 ############################################
 
 display_obj.fill(0) # Clears display once at the start
-border.border_draw(display_obj) # Draws border once at the start
+obstacle.draw(display_obj) # Draws border once at the start
 
 # If I implement these, I assume I can print them once before the loop and then
 # set up something to check, if I have hit the Target, for score update to happen
@@ -375,6 +361,11 @@ display_obj.text("Score", 0, 57, 1)
 display_obj.text("00001", 90, 57, 1)
 
 while True:
+    #### TESTING ####
+    world.world_test()
+    #################
+
+
     # 1. Inputs
     event = get_joystick_event()
     if event in actions: actions[event]()
@@ -389,10 +380,11 @@ while True:
     # display_obj.fill(0)   # No point having this in loop period, because border 
                             # rendering already did the job of clearing screen
     # border.border_draw(display_obj)
+    target.target_draw(display_obj)
     snek_body.draw(display_obj) # Handle the drawing logic
     # display_obj.text("Score", 0, 57, 1) # Placeholder for Score or something else
     # display_obj.text("00001", 90, 57, 1) # Placeholder for Score (digits)
     display_obj.show()
     snek_body.body_collision()
 
-    sleep(0.05)
+    sleep(0.25) # "Speed" for testing
